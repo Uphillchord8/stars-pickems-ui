cc4a74b283471c59863a9adec3ddad5bc7f0bd07
Uphillchord8 <Cole.Davidson8@gmail.com>
2025-07-16 18:16:41 -0400

    Create dashboardpage.jsx

---
+++ src/pages/dashboardpage.jsx
@@ -0,0 +1,10 @@
+import Leaderboard from '../components/leaderboard';
+
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <Leaderboard />
+    </div>
+  );
+}

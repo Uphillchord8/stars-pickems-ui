8cd83c2fd20bfbc4f02d8e0f4300150fb8b585b8
Uphillchord8 <Cole.Davidson8@gmail.com>
2025-07-16 18:18:13 -0400

    Update privateroute.jsx

--- src/components/privateroute.jsx
+++ src/components/privateroute.jsx
@@ -1,36 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/cuthcontext';
-import PrivateRoute from './components/privateoute';
-import LoginPage from './pages/loginpage';
-import PicksPage from './pages/pickspage';
-import DashboardPage from './pages/dashboardpage';
+import { useContext } from 'react';
+import { AuthContext } from '../context/authcontext';
+import { Navigate } from 'react-router-dom';
 
-function App() {
-  return (
-    <Router>
-      <AuthProvider>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/picks"
-            element={
-              <PrivateRoute>
-                <PicksPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <DashboardPage />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </AuthProvider>
-    </Router>
-  );
+export default function PrivateRoute({ children }) {
+  const { user } = useContext(AuthContext);
+  return user ? children : <Navigate to="/login" />;
 }
-
-export default App;
